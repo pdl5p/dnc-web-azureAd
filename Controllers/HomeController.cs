911d38e3282ce904b06ea31d420f93a291d43bbb
Paul Lucas <paul.lucas@fivep.com.au>
2017-01-06 13:21:42 +1100

    Added about view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,5 +9,9 @@ namespace ConsoleApplication
         public IActionResult Index(){
             return View();
         }
+
+        public IActionResult About(){
+            return View();
+        }
     }
 }
